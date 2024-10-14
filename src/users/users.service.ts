d6970fe3810ce04310e6a8ed32bb95644ddaac03
MilitsaTsvetkova <militsa88@yahoo.com>
2024-10-14 12:22:01 +0300

    user entity along with services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.repository.create({ email, password });
+    return this.repository.save(user);
+  }
+
+  find(email: string) {
+    return this.repository.find({
+      where: { email },
+    });
+  }
+
+  findOne(id: number) {
+    return this.repository.findOne({ where: { id } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.repository.remove(user);
+  }
+}
