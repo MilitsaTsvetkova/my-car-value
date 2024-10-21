09a9ddae55e1969617bd02ea6fc5960bc94f1c09
MilitsaTsvetkova <militsa88@yahoo.com>
2024-10-21 14:14:39 +0300

    delete db before each test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('', () => {
+describe('Authentication', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -19,12 +19,29 @@ describe('', () => {
     const email = 'asdf@asdf.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email, password: 'mypass' })
+      .send({ email, password: 'Mypass@123' })
       .expect(201)
       .then((response) => {
         const { id, email } = response.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'Mypass@123' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
